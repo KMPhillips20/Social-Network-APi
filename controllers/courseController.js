3162e378b41f6c7fa2aebaeff4ead5d09c6795d6
Kyle <kylemphillips65@yahoo.com>
2023-02-07 22:28:15 -0500

    setup userController and add function for get all users

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -1,54 +1,123 @@
-const { Course, Student } = require('../models');
+const { User, Thought } = require('../models');
 
 module.exports = {
-  // Get all courses
-  getCourses(req, res) {
-    Course.find()
-      .then((courses) => res.json(courses))
-      .catch((err) => res.status(500).json(err));
-  },
-  // Get a course
-  getSingleCourse(req, res) {
-    Course.findOne({ _id: req.params.courseId })
+  // ------------- Get All Users -----------
+  getUsers(req, res) {
+    User.find()
       .select('-__v')
-      .then((course) =>
-        !course
-          ? res.status(404).json({ message: 'No course with that ID' })
-          : res.json(course)
-      )
-      .catch((err) => res.status(500).json(err));
-  },
-  // Create a course
-  createCourse(req, res) {
-    Course.create(req.body)
-      .then((course) => res.json(course))
+      .then(async (users) => {
+        const userObj = {
+          users,
+        };
+        return res.json(userObj);
+      })
       .catch((err) => {
         console.log(err);
         return res.status(500).json(err);
       });
   },
-  // Delete a course
-  deleteCourse(req, res) {
-    Course.findOneAndDelete({ _id: req.params.courseId })
-      .then((course) =>
-        !course
-          ? res.status(404).json({ message: 'No course with that ID' })
-          : Student.deleteMany({ _id: { $in: course.students } })
+
+
+  // ---------- Get A Single User --------------
+  getSingleUser(req, res) {
+    User.findOne({ _id: req.params.userId })
+      .select('-__v')
+      .lean()
+      .then(async (user) =>
+        !user
+          ? res.status(404).json({ message: 'There is no user with that ID' })
+          : res.json(user)
       )
-      .then(() => res.json({ message: 'Course and students deleted!' }))
+      .catch((err) => {
+        console.log(err);
+        return res.status(500).json(err);
+      });
+  },
+
+
+  // ----------- Create A New User ---------------
+  createUser(req, res) {
+    User.create(req.body)
+      .then((user) => res.json(user))
       .catch((err) => res.status(500).json(err));
   },
-  // Update a course
-  updateCourse(req, res) {
-    Course.findOneAndUpdate(
-      { _id: req.params.courseId },
+
+  updateUser(req, res) {
+    User.findOneAndUpdate(
+      { id: req.params.userId },
       { $set: req.body },
-      { runValidators: true, new: true }
+      {
+        runValidators: true,
+        new: true
+      }
     )
-      .then((course) =>
-        !course
-          ? res.status(404).json({ message: 'No course with this id!' })
-          : res.json(course)
+      .then((user =>
+        !user
+          ? res.status(404).json({ message: 'There is no user with this id!' })
+          : res.json(user)
+      )
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+
+  
+  // ------------------ Delete a user and all their thoughts -----------------
+  deleteUser(req, res) {
+    User.findOneAndRemove({ _id: req.params.userId })
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: 'No such user exists' })
+          : User.findOneAndUpdate(
+            { users: req.params.userId },
+            { $pull: { users: req.params.userId } },
+            { new: true }
+          )
+      )
+      .then((user) =>
+        !user
+          ? res.status(404).json({
+            message: 'User was deleted, but no thoughts were found',
+          })
+          : res.json({ message: 'User was successfully deleted' })
+      )
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
+
+
+  // --------------- Add Someone -----------
+  addSomeone(req, res) {
+    console.log('You are adding someone');
+    console.log(req.body);
+    User.findOneAndUpdate(
+      { _id: req.params.userId },
+      { $addToSet: { someone: req.params.someoneId } },
+    )
+      .then((user) =>
+        !user
+          ? res
+            .status(404)
+            .json({ message: 'No user was found' })
+          : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+
+
+  // -------------- Remove Someone ------------
+  removeSomeone(req, res) {
+    User.findOneAndUpdate(
+      { _id: req.params.userId },
+      { $pull: { someone: req.params.someoneId } },
+    )
+      .then((user) =>
+        !user
+          ? res
+            .status(404)
+            .json({ message: 'No user was found' })
+          : res.json(user)
       )
       .catch((err) => res.status(500).json(err));
   },
