80566be2e859c789c7f3ac50ee348c838606ce90
Kyle <kylemphillips65@yahoo.com>
2023-02-07 23:37:41 -0500

    added function for create a thought

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -8,19 +8,46 @@ module.exports = {
   getThoughts(req, res) {
     Thought.find()
       .select('-__v')
-      .then((thoughts) => res.json(thoughts))
+      .then((thought) => res.json(thought))
       .catch((err) => res.status(500).json(err));
   },
 
-    // --------- Get Thought -----------
-    getSingleThought(req, res) {
-      Thought.findOne({ _id: req.params.thoughtId })
-        .select('-__v')
-        .then((thought) =>
-          !thought
-            ? res.status(404).json({ message: 'There are no thoughts with that ID' })
-            : res.json(thought)
-        )
-        .catch((err) => res.status(500).json(err));
-    },
+  // --------- Get Thought -----------
+  getSingleThought(req, res) {
+    Thought.findOne({ _id: req.params.thoughtId })
+      .select('-__v')
+      .then((thought) =>
+        !thought
+          ? res.status(404).json({ message: 'There are no thoughts with that ID' })
+          : res.json(thought)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+
+  // ----------- Create A Thought -------------
+  createThought(req, res) {
+    Thought.create(req.body)
+      .then((thought) => {
+        console.log(thought)
+        return User.findOneAndUpdate(
+          { _id: req.body.userId },
+          { $addToSet: { thought: thought._id } },
+          {
+            runValidators: false,
+            new: true
+          }
+        );
+      })
+      .then((user) =>
+        !user
+          ? res.status(404).json({
+            message: 'A thought has been created, but no user with ID how been found',
+          })
+          : res.json('Thought was created!')
+      )
+      .catch((err) => {
+        console.log(err);
+        return res.status(500).json(err);
+      });
+  },
 };
